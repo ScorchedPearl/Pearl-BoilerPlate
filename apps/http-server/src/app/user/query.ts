67777239ea3a6b3f3c0b4531ce230ecf747f50a3
vishwas0612 <IIT2024087@iiita.ac.in>
2025-01-02 21:45:59 +0530

    Fixed

--- apps/http-server/src/app/user/query.ts
+++ apps/http-server/src/app/user/query.ts
@@ -1,3 +1,3 @@
 export const query=`#graphql
-
+ getCurrentUser:User
 `
--- apps/http-server/src/app/user/resolver.ts
+++ apps/http-server/src/app/user/resolver.ts
@@ -1,5 +1,5 @@
 import { GraphqlContext } from "../../interfaces"
-import { prismaClient } from "@workspace/prisma/src/index";
+import { prismaClient } from "@workspace/prisma/build/index";
 const queries={
  getCurrentUser: async(parent:any, args:any, ctx:GraphqlContext)=>{
   const id=ctx.user?.id
